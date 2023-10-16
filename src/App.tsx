48506d507ff087499c3e3774d44ecbd4b0d2c29b
Sweet Dream <noahsflood908@gmail.com>
2023-10-16 04:52:51 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useMemo } from "react";
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, } from '@solana/wallet-adapter-react-ui';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { SnackbarProvider } from 'notistack';
+import {
+    getLedgerWallet,
+    getPhantomWallet,
+    getSlopeWallet,
+    getSolflareWallet,
+    getSolletExtensionWallet,
+    getSolletWallet,
+    getTorusWallet,
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import Home from './pages/home'
+
+import './bootstrap.min.css';
+import './chunk.css'
+import 'antd/dist/antd.css';
+import './assets/style.scss'
+
+export default function App(){
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [getPhantomWallet(),getSolletExtensionWallet(),getSolflareWallet(),getLedgerWallet()], []);
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <SnackbarProvider>
+              <Home></Home>
+            </SnackbarProvider>
+          </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );  
+}
